09c93b905f0893290ebf064e4c1ab1a842cea66f
Luis Orostizaga <luis.orostizaga@gmail.com>
2020-01-27 20:55:34 -0300

    Project Upload
    Aplicación de Consola que realice haciendo pruebas con Firestore, por si a alguien le sirve.

---
+++ TestGoogleFireStore/FireStoreApiImplementation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Google.Cloud.Firestore;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace TestGoogleFireStore
+{
+    partial class Program
+    {
+        public static class FireStoreApiImplementation
+        {
+
+            public static async void callAddDataHelper(FirestoreDb db)
+            {
+                await AddData1(db);
+            }
+
+            public static async Task AddData1(FirestoreDb db)
+            {
+                //FirestoreDb db = FirestoreDb.Create(project);
+
+                DocumentReference docRef = db.Collection("users").Document("aturing");
+                Dictionary<string, object> user = new Dictionary<string, object>
+                {
+                    { "First", "Alan" },
+                    { "Middle", "Mathison" },
+                    { "Last", "Turing" },
+                    { "Born", 1912 }
+                };
+                await docRef.SetAsync(user);
+                Console.WriteLine("Added data to the alovelace document in the users collection.");
+            }
+
+            public static async void callRetrieveDataHelper(FirestoreDb db)
+            {
+                await RetrieveAllDocuments(db);
+            }
+
+            private static async Task RetrieveAllDocuments(FirestoreDb db)
+            {
+                //FirestoreDb db = FirestoreDb.Create(project);
+                CollectionReference usersRef = db.Collection("users");
+                QuerySnapshot snapshot = await usersRef.GetSnapshotAsync();
+                foreach (DocumentSnapshot document in snapshot.Documents)
+                {
+                    Console.WriteLine("User: {0}", document.Id);
+                    Dictionary<string, object> documentDictionary = document.ToDictionary();
+                    Console.WriteLine("First: {0}", documentDictionary["First"]);
+                    if (documentDictionary.ContainsKey("Middle"))
+                    {
+                        Console.WriteLine("Middle: {0}", documentDictionary["Middle"]);
+                    }
+                    Console.WriteLine("Last: {0}", documentDictionary["Last"]);
+                    Console.WriteLine("Born: {0}", documentDictionary["Born"]);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
---
+++ TestGoogleFireStore/Program.cs
@@ -0,0 +1,43 @@
+﻿using Google.Apis.Auth.OAuth2;
+using System;
+using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+using Grpc.Core;
+using Grpc.Auth;
+
+namespace TestGoogleFireStore
+{
+    partial class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+            string project = "YOUR_PROJECT_ID";
+
+
+            GoogleCredential cred = GoogleCredential.FromFile(@"C:\[YOUR_PATH]\[YOUR_FILE].json");
+            Channel channel = new Channel(FirestoreClient.DefaultEndpoint.Host,
+                          FirestoreClient.DefaultEndpoint.Port,
+                          cred.ToChannelCredentials());
+            FirestoreClient client = FirestoreClient.Create(channel);
+            FirestoreDb db = FirestoreDb.Create(project, client);
+            Console.WriteLine("Created Cloud Firestore client with project ID: {0}", project);
+
+            //GoogleApiImplement.callAddDataHelper(db);
+            FireStoreApiImplementation.callRetrieveDataHelper(db);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+
+
+
+
